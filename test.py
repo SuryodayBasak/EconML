38fc44696ddb10a7b22a04ba2a13b72d03a6dd3b
SuryodayBasak <suryodaybasak@gmail.com>
2017-07-16 16:40:18 +0530

    calculated log of all values of the training data

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy as np
 
 data_file = 'iris.csv'
-DATA = pd.read_csv(data_file, sep = ',', header=0)
-DATA['l'] = DATA['l'].map({'Iris-setosa':0, 'Iris-versicolor':1, 'Iris-virginica':2})
+data_frame = pd.read_csv(data_file, sep = ',', header=0)
+data_frame['l'] = data_frame['l'].map({'Iris-setosa':1, 'Iris-versicolor':2, 'Iris-virginica':3})
+
+DATASET = np.log(data_frame.values[:, 0:-1])
+LABELS = data_frame.values[:, 4]
 
