7dea222b9f5355e2a21bd0e1b5354c4d6d68ee92
SuryodayBasak <suryodaybasak@gmail.com>
2017-07-17 01:30:02 +0530

    linear regression not working

--- ProductionFunctions.py
+++ ProductionFunctions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+from random import random as rand
 import theano
 from theano import function
 from theano import pp
@@ -9,6 +10,7 @@ from theano import tensor as T
 class CobbDouglas:
 	def __init__(self, data, labels):
 		self.DATA = np.log(data)
+		print(self.DATA)
 		self.LABELS = labels
 	
 		self.learning_rate = 0.1
@@ -18,8 +20,10 @@ class CobbDouglas:
 	def findRegressionCoefficients(self):
 
 		self.x = T.matrix(name='x')  #Input matrix with examples.
+		self.c = theano.shared(value = 0.0, dtype=theano.config.floatX), name='w', borrow=True)  #Constant of regression
 		self.w = theano.shared(value = np.zeros((self.n_dims, 1), dtype=theano.config.floatX), name='w', borrow=True)
 		self.f = function([self.x], T.dot(self.w, self.x)) # Linear regression.
+		#self.f = function([self.x], T.dot(self.w, self.x) + self.c) # Linear regression.
 		
 		self.y = T.vector(name='y')  # Output vector with y values.
 		self.loss = T.mean((T.dot(self.x, self.w).T - self.y) ** 2) # Define loss function.
@@ -37,3 +41,39 @@ class CobbDouglas:
 		
 		#print(self.w.eval())
 		return np.squeeze(np.asarray(self.w.eval()))
+
+def randomSample(DATASET, LABELS, ratio):
+	if np.shape(DATASET)[0] != np.shape(LABELS)[0]:
+		print("The size of the feature vectors and target variables are different.")
+	
+	training_data = []
+	training_labels = []
+	test_data = []
+	test_labels = []
+	
+	for i in range(len(DATASET)):
+		r = rand()
+		if r < ratio:
+			training_data.append(DATASET[i])
+			training_labels.append(LABELS[i])
+		else:
+			test_data.append(DATASET[i])
+			test_labels.append(LABELS[i])
+	
+	return np.matrix(training_data), np.array(training_labels), np.matrix(test_data), np.array(test_labels)
+
+#WRITE THIS IN THEANO LATER
+def elasticExponentiation(DATASET, ELASTICITIES):
+	if np.shape(DATASET)[1] != np.shape(ELASTICITIES)[0]:
+		print("Lengths of feature vectors and easticity vector are not the same")
+	
+	rows, cols = np.shape(DATASET)
+	results = []
+	for i in range(rows):
+		answer = 1
+		for j in range(cols):
+			answer*= DATASET[i,j]**ELASTICITIES[j]
+		results.append([answer])
+	
+	return results
+	
--- test.py
+++ test.py
@@ -3,16 +3,57 @@ import numpy as np
 import theano
 import theano.tensor as T
 import ProductionFunctions as pf
+from sklearn import neighbors
 
+def findAccuracy(predicted, original):	#make this single line, if needed, or extend it to calculate other metrics
+	N = len(predicted)
+	accuracy = 0.0
+	for i in range(N):
+		if predicted[i] == original[i]:
+			accuracy += 1
+	return accuracy/N
+		
+		
 data_file = 'iris.csv'
+data_file = 'skin_seg.csv'
 data_frame = pd.read_csv(data_file, sep = ',', header=0)
-data_frame['l'] = data_frame['l'].map({'Iris-setosa':1, 'Iris-versicolor':2, 'Iris-virginica':3})
+#data_frame['l'] = data_frame['l'].map({'Iris-setosa':2, 'Iris-versicolor':3, 'Iris-virginica':4})
 
 DATASET = data_frame.values[:, 0:-1]
-LABELS = data_frame.values[:, 4]
-prep = pf.CobbDouglas(DATASET, LABELS)
-ELASTICITIES = prep.findRegressionCoefficients()
-
-print(np.dot(DATASET, ELASTICITIES))
-
+LABELS = data_frame.values[:, 3]
 ############################################################################################
+
+print(DATASET)
+print(LABELS)
+
+results_original = []
+results_processed = []
+
+for iteration in range(0, 10):
+	print('ITERATION = ', iteration+1)
+	
+	training_data, training_labels, test_data, test_labels = pf.randomSample(DATASET, LABELS, 0.8)
+	prep = pf.CobbDouglas(training_data, training_labels)
+	ELASTICITIES = prep.findRegressionCoefficients()
+	
+	#PROCESSED_TRAINING_DATA = np.reshape(np.dot(training_data, ELASTICITIES), (np.shape(training_data)[0],1))
+	#PROCESSED_TEST_DATA = np.reshape(np.dot(test_data, ELASTICITIES), (np.shape(test_data)[0],1))
+	PROCESSED_TRAINING_DATA = pf.elasticExponentiation(training_data, ELASTICITIES)
+	PROCESSED_TEST_DATA = pf.elasticExponentiation(test_data, ELASTICITIES)
+	
+	print(ELASTICITIES)
+		
+	#Model implementation on original data
+	clf = neighbors.KNeighborsClassifier(7)
+	clf.fit(training_data, training_labels)
+	predicted = clf.predict(test_data)
+	results_original.append(findAccuracy(predicted, test_labels))
+    
+	#Model implementation on processed data
+	clf_processed = neighbors.KNeighborsClassifier(7)
+	clf_processed.fit(PROCESSED_TRAINING_DATA, training_labels)
+	predicted_processed = clf_processed.predict(PROCESSED_TEST_DATA)
+	results_processed.append(findAccuracy(predicted_processed, test_labels))
+
+print('Results on original data: ', sum(results_original)/len(results_original))
+print('Results on processed data: ', sum(results_processed)/len(results_processed))
