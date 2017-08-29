189a686976d7fbae487f5ed5f4357a2dd3d1fce7
SuryodayBasak <suryodaybasak@gmail.com>
2017-08-29 18:05:53 +0530

    Building up SVM

--- SVM_master_script.py
+++ SVM_master_script.py
@@ -1,3 +1,5 @@
+#code is learned from sentdex
+
 import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
@@ -13,8 +15,39 @@ class support_vector_machine:
 			self.ax = self.fig.add_subplot(1, 1, 1)
 			
 	def fit(self, data):
-		pass
+		self.data = data
+		#{||w|| = [w,b]}
+		opt_dict = {}
+		transforms = [[1,1], [-1,1],[-1,-1],[1,-1]]
 		
+		#replace this block of code - make it shorter: just find min and max feature values
+		all_data = []
+		for yi in self.data:
+			for featureset in self.data[yi]:
+				for feature in featureset:
+					all_data.append(feature)
+					
+		self.max_feature_value = max(all_data)
+		self.min_feature_value = min(all_data)
+		all_data = None
+		
+		step_sizes = [self.max_feature_value * 0.1,					#think of a different way to do this
+						self.max_feature_value * 0.01,
+						self.max_feature_value * 0.001]
+		
+		#extremely expensive
+		b_range_multiple = 5
+		#
+		b_multiple = 5
+		latest_optimum = self.max_feature_value * 10
+		
+		for step in step_sizes:
+			w = np.array([latest_optimum, latest_optimum])
+			optimized = False
+			
+			while not optimized:
+				pass
+			
 	def predict(self, features):
 		#sign(x.w + b)
 		classification = np.sign(np.dot(np.array(features), self.w) + self.b)
